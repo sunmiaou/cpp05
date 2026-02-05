15f6e178d3a024b4b74fb3c548e46ec5ce17d080
zitriss <zitrissonline@gmail.com>
2026-02-05 16:36:15 +0100

    reset pc

--- ex00/includes/Bureaucrat.hpp
+++ ex00/includes/Bureaucrat.hpp
@@ -6,7 +6,32 @@
 /*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 16:42:50 by tlize             #+#    #+#             */
-/*   Updated: 2026/01/29 16:42:51 by tlize            ###   ########.fr       */
+/*   Updated: 2026/02/02 19:24:13 by tlize            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+public:
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat &old_crat);
+	Bureaucrat &operator=(const Bureaucrat &old_crat);
+	~Bureaucrat();
+
+	const std::string	getName();
+	int					getGrade();
+	void				incrementGrade();
+	void				decrementGrade();
+private:
+	const std::string	name;
+	int					grade;
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &value);
+
+#endif
