dde9209431c8366e508eb0282a99f6b005802aac
sunmiaou <130472899+sunmiaou@users.noreply.github.com>
2026-05-26 20:55:00 +0200

    default constructor and getters

--- ex00/src/Bureaucrat.cpp
+++ ex00/src/Bureaucrat.cpp
@@ -6,7 +6,23 @@
 /*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 16:42:54 by tlize             #+#    #+#             */
-/*   Updated: 2026/01/29 16:42:54 by tlize            ###   ########.fr       */
+/*   Updated: 2026/05/26 20:52:17 by tlize            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(): name("DefaultName"), grade(150)
+{
+    std::cout << "Bureaucrat default constructor called" << std::endl;
+}
+
+const std::string Bureaucrat::getName()
+{
+	return (this->name);
+}
+
+int Bureaucrat::getGrade()
+{
+	return (this->grade);
+}
