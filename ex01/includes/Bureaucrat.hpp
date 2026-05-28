287247fb56eadaf3af7141afc311f4afa35f4647
sunmiaou <130472899+sunmiaou@users.noreply.github.com>
2026-05-28 16:46:41 +0200

    init ex01 and final push ex00

---
+++ ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 16:42:50 by tlize             #+#    #+#             */
+/*   Updated: 2026/05/26 22:50:00 by tlize            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+public:
+	Bureaucrat();
+	Bureaucrat(std::string givenName, int givenGrade);
+	Bureaucrat(const Bureaucrat &old_crat);
+	Bureaucrat &operator=(const Bureaucrat &old_crat);
+	~Bureaucrat();
+
+	const std::string	getName() const;
+	int					getGrade() const;
+	void				incrementGrade();
+	void				decrementGrade();
+
+	//Exception
+	class GradeException: public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+private:
+	void				applyGrade(int grade);
+
+	const std::string	name;
+	int					grade;
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &value);
+
+#endif
